4df062181f8f09112e8f64c703a55a0fdc1a8d11
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-01-22 17:44:09 +0200

    create simple asgi compatible app for lifespan and http protocols

--- app/main.py
+++ app/main.py
@@ -1,10 +1,57 @@
-from starlette.responses import HTMLResponse
+import json
+import sqlite3
+from pathlib import Path
 
 
 async def app(scope, receive, send):
-    assert scope["type"] == "http"
-    if scope["path"] == "/":
-        response = HTMLResponse("Hello world!")
+    if scope["type"] == "lifespan":
+        while True:
+            msg = await receive()
+            if msg["type"] == "lifespan.startup":
+                scope["state"]["db"] = sqlite3.connect(Path.cwd() / "db.sqlite")
+                await send({"type": "lifespan.startup.complete"})
+            elif msg["type"] == "lifespan.shutdown":
+                scope["state"]["db"].close()
+                await send({"type": "lifespan.shutdown.complete"})
+                break
     else:
-        response = HTMLResponse("Not found", 404)
-    await response(scope, receive, send)
+        assert scope["type"] == "http"
+        content_type = next(
+            (value for header, value in scope["headers"] if header == b"content-type"),
+            None,
+        )
+        assert content_type == b"application/json"
+        db = scope["state"]["db"]
+
+        msg = await receive()
+        data = json.loads(msg["body"])
+
+        if "username" in data and "password" in data:
+            cur = db.cursor()
+            cur.execute(
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (data["username"], data["password"]),
+            )
+            db.commit()
+            cur.close()
+
+            status = 201
+            response = b"user is successfully registered"
+        else:
+            status = 422
+            response = b"'username' and 'password' are required"
+
+        await send(
+            {
+                "type": "http.response.start",
+                "status": status,
+                "headers": [(b"content-type", b"text/plain")],
+            }
+        )
+        await send(
+            {
+                "type": "http.response.body",
+                "body": response,
+                "more_body": False,
+            }
+        )
