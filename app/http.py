c6fe578bcbe9bd124929d6454238adfb0219b16c
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-01-24 16:07:42 +0200

    add templating and update requirements

--- app/http.py
+++ app/http.py
@@ -1,14 +1,14 @@
 from starlette.endpoints import HTTPEndpoint
 from starlette.requests import Request
-from starlette.responses import PlainTextResponse
 from starlette.routing import Mount, Route
 
 from app.auth import routes as auth_routes
+from app.templating import templates
 
 
 class Index(HTTPEndpoint):
     def get(self, request: Request):
-        return PlainTextResponse("Hello world!")
+        return templates.TemplateResponse(request, "index.html")
 
 
 routes = [
