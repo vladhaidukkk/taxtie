3fbcc04a03d259f6c6014cb09e1e9a86ffede9e4
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-01-25 14:15:23 +0200

    handle graphql requests by hands

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-from starlette.routing import Mount
+from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 
 from app import settings
+from app.graphql import endpoint as graphql_endpoint
 from app.http import routes as http_routes
 from app.lifespan import lifespan
 from app.middlewares import PrintClientMiddleware
@@ -15,11 +16,12 @@ app = Starlette(
     lifespan=lifespan,
     middleware=[
         Middleware(PrintClientMiddleware),
-        Middleware(CORSMiddleware),
+        Middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"]),
         Middleware(SessionMiddleware, secret_key=settings.SECRET_KEY),
     ],
     routes=[
         *http_routes,
+        Route("/graphql", graphql_endpoint, methods=["POST"]),
         Mount("/ws", routes=ws_routes, name="ws"),
         Mount("/static", StaticFiles(directory="static", html=True), name="static"),
     ],
