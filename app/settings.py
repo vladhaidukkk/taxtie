5811fa30143728996216bef1967718e19a877023
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-01-27 08:59:26 +0200

    clear the project to start development

--- app/settings.py
+++
@@ -1,9 +0,0 @@
-from databases import DatabaseURL
-from starlette.config import Config
-from starlette.datastructures import Secret
-
-config = Config(".env")
-
-DEBUG = config("DEBUG", cast=bool, default=False)
-SECRET_KEY = config("SECRET_KEY", cast=Secret)
-DATABASE_URL = config("DATABASE_URL", cast=DatabaseURL)
